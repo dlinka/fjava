50d4f3b01ec2610edc3a08228fe6a2b4718f3075
dlinka <dlinka.cg@gmail.com>
2020-05-17 13:06:13 +0800

    遍历Map

--- src/main/java/com/cr/map/LoopMap.java
+++ src/main/java/com/cr/map/LoopMap.java
@@ -6,35 +6,37 @@ import java.util.Map;
 
 public class LoopMap {
 
-    private void loop1(){
-        Map<String, Integer> map = new HashMap<>();
-        for(Map.Entry<String, Integer> entry : map.entrySet()){
-            //entry.getKey()
-            //entry.getValue()
+    Map<String, Integer> map = new HashMap<>();
+
+    public void loop1() {
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            entry.getKey();
+            entry.getValue();
         }
     }
 
-    private void loop2(){
-        Map<String, Integer> map = new HashMap<>();
-        for(String key : map.keySet()){}
-        for(Integer value : map.values()){}
+    public void loop2() {
+        for (String key : map.keySet()) {
+        }
+        for (Integer value : map.values()) {
+        }
     }
 
-    private void loop3(){
-        Map<String, Integer> map = new HashMap<>();
+    public void loop3() {
         Iterator<Map.Entry<String, Integer>> iterator = map.entrySet().iterator();
-        while(iterator.hasNext()){
+        while (iterator.hasNext()) {
             Map.Entry<String, Integer> entry = iterator.next();
-            //entry.getKey()
-            //entry.getValue()
+            entry.getKey();
+            entry.getValue();
         }
         Iterator<String> iteratorKey = map.keySet().iterator();
-        while(iteratorKey.hasNext()){
-            //iteratorKey.next()
+        while (iteratorKey.hasNext()) {
+            iteratorKey.next();
         }
         Iterator<Integer> iteratorValue = map.values().iterator();
-        while(iteratorValue.hasNext()){
-            //iteratorValue.next()
+        while (iteratorValue.hasNext()) {
+            iteratorValue.next();
         }
     }
+
 }
