426a6f56405a04f8e9a80bddd393145e0b1b1b2f
dlinka <dlinka.cg@gmail.com>
2020-05-17 14:00:14 +0800

    移位运算符

---
+++ src/main/java/com/cr/bit/BitOperation.java
@@ -0,0 +1,38 @@
+package com.cr.bit;
+
+public class BitOperation {
+
+    public static void main(String[] args) {
+        BitOperation bo = new BitOperation();
+        bo.leftMove();
+        bo.rightMove();
+        bo.noSymbolRightMove();
+    }
+
+    //补码进行左移n位
+    //符号位也移动
+    //右边补零
+    //移动的位数如果大于等于32位,就对32取余
+    //移动32位,相当于不移动,移动40位,相当于移动8位
+    public void leftMove() {
+        int num = 12333292;
+        System.out.println(num << 8);
+    }
+
+    //补码进行右移n位
+    //左边补上符号位
+    //正数全补上0,负数全补上1
+    //移动的位数如果大于等于32位,也对32取余
+    public void rightMove() {
+        int num = 12333292;
+        System.out.println(num >> 8);
+    }
+
+    //补码进行右移n位
+    //左边全补上0
+    //移动的位数如果大于等于32位,也对32取余
+    public void noSymbolRightMove() {
+        int num = -1;
+        System.out.println(num >>> 8);
+    }
+}
