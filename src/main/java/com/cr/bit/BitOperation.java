1ae756492b1f80909ff59e49f1c653d828e038bf
dlinka <dlinka.cg@gmail.com>
2020-05-18 00:33:46 +0800

    位运算

--- src/main/java/com/cr/bit/BitOperation.java
+++ src/main/java/com/cr/bit/BitOperation.java
@@ -2,37 +2,37 @@ package com.cr.bit;
 
 public class BitOperation {
 
+    //与运算
+    //&
+    //两个位都为1时,才为1
+    public void bitAND() {
+        System.out.println(1 & 1); //1
+        System.out.println(1 & 0); //0
+        System.out.println(0 & 0); //0
+    }
+
+    //或运算
+    //|
+    //两个位都为0时,才为0
+    public void bitOR() {
+        System.out.println(1 | 1); //1
+        System.out.println(1 | 0); //1
+        System.out.println(0 | 0); //0
+    }
+
+    //异或运算
+    //^
+    //两个位相同为0,相异为1
+    public void bitXOR() {
+        System.out.println(1 ^ 1); //0
+        System.out.println(0 ^ 0); //0
+        System.out.println(1 ^ 0); //1
+    }
+
     public static void main(String[] args) {
         BitOperation bo = new BitOperation();
-        bo.leftMove();
-        bo.rightMove();
-        bo.noSymbolRightMove();
-    }
-
-    //补码进行左移n位
-    //符号位也移动
-    //右边补零
-    //移动的位数如果大于等于32位,就对32取余
-    //移动32位,相当于不移动,移动40位,相当于移动8位
-    public void leftMove() {
-        int num = 12333292;
-        System.out.println(num << 8);
-    }
-
-    //补码进行右移n位
-    //左边补上符号位
-    //正数全补上0,负数全补上1
-    //移动的位数如果大于等于32位,也对32取余
-    public void rightMove() {
-        int num = 12333292;
-        System.out.println(num >> 8);
-    }
-
-    //补码进行右移n位
-    //左边全补上0
-    //移动的位数如果大于等于32位,也对32取余
-    public void noSymbolRightMove() {
-        int num = -1;
-        System.out.println(num >>> 8);
+        bo.bitAND();
+        bo.bitOR();
+        bo.bitXOR();
     }
 }
