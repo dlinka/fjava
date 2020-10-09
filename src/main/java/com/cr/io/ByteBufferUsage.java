e4def6c16c7d7ebd8dd15bc79fadfc790b80eb55
cyg <dlinka.cg@gmail.com>
2020-10-09 10:38:07 +0800

    ByteBuffer用法

---
+++ src/main/java/com/cr/io/ByteBufferUsage.java
@@ -0,0 +1,67 @@
+package com.cr.io;
+
+import java.nio.ByteBuffer;
+
+public class ByteBufferUsage {
+
+    public static void main(String[] args) {
+        //init();
+        //putget();
+        //loop();
+        slice();
+    }
+
+    //初始化
+    private static void init() {
+        //1
+        ByteBuffer buffer1 = ByteBuffer.allocate(10);
+
+        //2
+        byte[] arr = new byte[10];
+        ByteBuffer buffer2 = ByteBuffer.wrap(arr);
+    }
+
+    private static void putget(){
+        ByteBuffer buffer = ByteBuffer.allocate(10);
+        buffer.putInt(10);
+        //4
+        System.out.println(buffer.position());
+        //10
+        System.out.println(buffer.limit());
+
+        //get之前需要置位
+        buffer.flip();
+
+        //0
+        System.out.println(buffer.position());
+        //4
+        System.out.println(buffer.limit());
+        System.out.println(buffer.getInt());
+    }
+
+    private static void loop() {
+        ByteBuffer buffer;
+        byte[] num = new byte[10];
+        for (int i = 0; i < num.length; i++) {
+            num[i] = (byte) (i+1);
+        }
+        buffer = ByteBuffer.wrap(num);
+        while(buffer.hasRemaining()){
+            System.out.println(buffer.get());
+        }
+    }
+
+    private static void slice() {
+        ByteBuffer buffer = ByteBuffer.allocate(10);
+        buffer.putInt(10);
+
+        ByteBuffer slice = buffer.slice();
+        //0
+        System.out.println(slice.position());
+        //6
+        System.out.println(slice.limit());
+        //6
+        System.out.println(slice.capacity());
+    }
+
+}
